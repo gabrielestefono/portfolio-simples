7d86d811ec1af8154ce9b0d1f666c952eb375554
gabrielestefono <gabrielestefono@hotmail.com>
2023-04-04 13:10:22 -0300

    JavaScript - Botão hamburger

---
+++ assets/js/script.js
@@ -0,0 +1,27 @@
+let links = document.getElementsByClassName('links');
+let show = document.getElementById('show');
+let hamburger = document.getElementById('hamburger');
+
+let ajustarTela = ()=>{
+    if(screen.width <= 768){
+        Object.values(links).forEach(value=>{
+            value.classList = 'header__nav--ul_li-a-ativo links link';
+        })
+    }else{
+        Object.values(links).forEach(value=>{
+            value.classList = 'header__nav--ul_li-a links link';
+        })
+    }
+}
+
+window.onresize = ajustarTela
+
+hamburger.addEventListener('click', ()=>{
+    if(show.className == 'header__nav--ul'){
+        show.className = 'header__nav--ul-ativo'
+    }else{
+        show.className = 'header__nav--ul'
+    }
+})
+
+ajustarTela();
